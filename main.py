71079440514101d8ec5c2daf9814a6d82ab81ddb
boxy-pug <wewepipi99@pm.me>
2024-04-07 01:10:25 +0200

    finished printing sorted letter count

--- main.py
+++ main.py
@@ -3,7 +3,12 @@ def main():
     text = get_book_path(book_path)
     word_count = get_word_count(text)
     letter_count = get_letter_count(text)
-    print(f"Word count: {word_count}\nLetter count: {letter_count}")
+    sorted_letter_count = get_sorted_letter_count(letter_count)
+
+    print(f"-- Book report of {book_path} --")
+    print(f"Word count: {word_count}\n")
+    print_letter_count(sorted_letter_count)
+    print("\n-- End of report --")
 
 
 def get_word_count(string):
@@ -14,7 +19,7 @@ def get_word_count(string):
 def get_book_path(path):
     with open(path) as f:
         return f.read()
-    
+
 
 def get_letter_count(text):
     lowered_text = text.lower()
@@ -25,6 +30,21 @@ def get_letter_count(text):
         else:
             letter_count[char] += 1
     return letter_count
+
+
+def get_sorted_letter_count(dict):
+    only_letters = {}
+    for key, value in dict.items():
+        if key.isalpha():
+            only_letters[key] = value
+
+    sorted_tuples = sorted(only_letters.items(), reverse=True, key=lambda item: item[1])
+    return sorted_tuples
+
+
+def print_letter_count(list):
+    for i in range(0, len(list)):
+        print(f"The letter {list[i][0]} was found {list[i][1]} times.")
 
 
 main()
