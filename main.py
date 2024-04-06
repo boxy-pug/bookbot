2203b448ade31ab7966fddab9cb7c1e06bbe5fe8
boxy-pug <wewepipi99@pm.me>
2024-04-06 14:14:31 +0200

    added word count

---
+++ main.py
@@ -0,0 +1,19 @@
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_path(book_path)
+    word_count = get_word_count(text)
+    print(f"Word count: {word_count}")
+
+        
+def get_word_count(string):
+    words = string.split(" ")
+    #for word in words:
+          #  if word 
+    return len(words)
+
+def get_book_path(path):
+        with open(path) as f:
+            return f.read()
+
+
+main()
